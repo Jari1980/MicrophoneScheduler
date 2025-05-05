a1d9b4ab66874143315ef513dfa38a6dd0479324
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-05-05 11:24:02 +0200

    initial

---
+++ src/main/java/org/workshop/microphoneschedulerapi/MicrophoneSchedulerApiApplication.java
@@ -0,0 +1,13 @@
+package org.workshop.microphoneschedulerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrophoneSchedulerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrophoneSchedulerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/workshop/microphoneschedulerapi/MicrophoneSchedulerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.workshop.microphoneschedulerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrophoneSchedulerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
