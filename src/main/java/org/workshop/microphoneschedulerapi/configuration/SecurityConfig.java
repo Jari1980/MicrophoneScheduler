685138be1563acd08ae279d0406d595cfdf94cd6
Jari Tenhunen <jari.tenhunen@outlook.com>
2025-06-16 10:07:50 +0200

    Add act added according to specs

--- src/main/java/org/workshop/microphoneschedulerapi/configuration/SecurityConfig.java
+++ src/main/java/org/workshop/microphoneschedulerapi/configuration/SecurityConfig.java
@@ -102,7 +102,8 @@ public class SecurityConfig {
                                 "api/v1/admin/editPersonage",
                                 "api/v1/admin/deletePersonage",
                                 "api/v1/admin/getActorsConnectedToUsers",
-                                "api/v1/admin/getCustomSceneList"
+                                "api/v1/admin/getCustomSceneList",
+                                "api/v1/admin/addAct"
                         ).hasRole(ADMINISTRATOR.name()))
 
                 .authorizeHttpRequests(registry ->
